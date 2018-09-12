91f02d9c6708bd424c3832de00b0b73a75afe8fe
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-09-12 13:33:02 +0000

    lesson1

---
+++ script.js
@@ -0,0 +1,18 @@
+// Tutorial 1 Intro...
+
+/*
+
+Overview of Node
+-A platform in which allow to run js to be able to run on a computer
+
+-Easier to read files easier
+-Can delete update or just upload files more efficently
+
+
+What to ahcieve from this lesson
+
+Learn the inner working of node
+Create a web browser
+Make a node.js app
+
+*/
