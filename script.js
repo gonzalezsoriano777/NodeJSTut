de5890034b8fde57bf5b9872e7ca7c9aae69750b
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-09-13 22:39:39 +0000

    lesson13

--- script.js
+++ script.js
@@ -210,24 +210,7 @@ Status // how to browser is doing
 
 */
 
-// How to create the server
-/*
 
-var http = require('http');
-
-var server = http.createServer(function(req, res){ // Takes to parameters (request and responce)
-     console.log('request was made: ' + req.url);   
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('Hey ninjas');
-   
-                                                   
-});
-
-
-server.listen(3000, '127.0.0.1'); // IP Address plus the 3000  calls the console
-console.log('yo dawgs, now listening to port 3000'); // this is how you set up a server in node.js
-
-*/
 
 
 
