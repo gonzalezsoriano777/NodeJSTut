d6476a6132a1ebd56c7d214cca3e00467d5f5def
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-09-14 00:32:30 +0000

    Update

--- NodeCode/script.js
+++ NodeCode/script.js
@@ -1,20 +1,14 @@
 // Tutorial 1 Intro...
 
 /*
-
 Overview of Node
 -A platform in which allow to run js to be able to run on a computer
-
 -Easier to read files easier
 -Can delete update or just upload files more efficently
-
-
 What to ahcieve from this lesson
-
 Learn the inner working of node
 Create a web browser
 Make a node.js app
-
 */
 
 
@@ -25,7 +19,6 @@ Make a node.js app
 // Turtorial 3, The v8 Engine
 
 /*
-
  Js Engines, don't understand Js infact it take JS and converts it to something
  More understandable for it, which is Machine Code
  
@@ -33,7 +26,6 @@ Make a node.js app
 
 // Understanding the process of v8 convertion
 /*
-
 Node.js is written in C++ ,its because it uses the v8 engine (one of js engines) which is actually written in C++ 
  So that it can convert to the js engine and that converts it to machine code
  
@@ -49,9 +41,7 @@ Node.js is written in C++ ,its because it uses the v8 engine (one of js engines)
 
 
 /*
-
 var time = 0;
-
 var timer = setInterval(function(){ // Updates how many seconds have passed each time, 
 //and it keep running
     time += 2;
@@ -60,17 +50,14 @@ var timer = setInterval(function(){ // Updates how many seconds have passed each
         clearInterval(timer);
     }
 }, 2000);
-
 */
 
 
 // Tutorial 5, Function Expressions
 
 /*
-
 Explaining function expressions
 As well as show, how to move a expression to a recent fucntion(shown in the file, 'app.js')
-
 */
 
 // Tutorial 6, Modules and Require()
@@ -79,108 +66,73 @@ As well as show, how to move a expression to a recent fucntion(shown in the file
 var counter = function(arr){
     return 'There are ' + arr.length + ' elements in this array';
 };
-
-
 module.exports = counter; // exports the counter as a module and connects it the 'app.js' variable of counter
 */
 
 // Tutorial 7, Module Patterns
 
 /*
-
 var counter = function(arr){
     return 'There are ' + arr.length + ' elements in this array';
     
 };
-
-
 var adder = function(a,b){
     return `The sum of the 2 numbers is $(a+b)`;// template strings.. 
 };
-
 var pi = 3.142;
-
 module.exports = { // stil doing the module exports but making more simplier to read
     counter: counter,
     adder: adder,
     pi: pi
 }
-
 module.exports = counter;
 // ^ this has always been an empty object
 module.exports.counter = counter;
 module.exports.adder = counter;
 module.exports.pi = counter;
 // ^ this is exporting the variable you defined
-
 // Easier way to use the module-exports is by replacing the var in each defined variable and use the module export
-
-
-
-
-
 */
 
 // Tutorial 8, The Node Event Emitter
 
 /*
-
 var events = require('events');
 var util = require('util');
-
 var Person = function(name){
     this.name = name;
 }
-
 util.inherits(Person, events.EventEmitter);
-
-
 var james = new Person('james');
 var mary = new Person('mary');
 var ryu = new Person('ryu');
-
 var people = [james, mary, ryu];
-
 people.forEach(function(person){
    person.on('speak', function(mssg){
        console.log(person.name + ' said: ' + mssg);
    });  // Takes whatever person object and attaching it the listener. 
 });
-
-
-
 james.emit('speak', 'hey dudes'); // using the variable james and make him speak and say 'hey dudes'
 ryu.emit('speak', 'I want a curry'); // ^ Same Process
 ////////////////////////////////////
-
-
 element.on('click', function(){})
-
 var myEmitter = new events.EvenEmitter();
-
 myEmitter.on('someEvent', function(mssg){
    console.log(mssg); });
-
 myEmitter.emit('someEvent', 'the event was emitted'); // This event is emmitted and the string is moved towards the function 
 //and it fires back to the console
-
-
 */
 
 // Tutorial 9, Reading & Writing Files (fs)
 
 /*
-
 Everything done app.js in this section
-
 */
 
 // Tutorial 10, Creating & Removing Directions
 
 /*
  Used App.js for this Tutorial
-
-
 */
 
 // Tutorial 11 , Clients and Servers
@@ -188,74 +140,42 @@ Everything done app.js in this section
 
 //Notes
 /*
-
 Protocals
 A set of communications rules, that two sides agree to use when communicating
-
 Ports
 A Program running on a computer that can listen for requests sent to a particular port number
-
 Ex... An IP Address, 172.24.86.76:3000
-
 */
 
 // Tutorial 12, Creating a server
 
 //Notes
 /*
-
 Responce Headers
 Content-Type // what you put in the browser
 Status // how to browser is doing
-
 */
 
 // Tutorial 13, Streams and Buffers
 
 // Notes
 /*
-
 Buffers
-
 -Temporary Storage spot for a chunk of data that is being transferred from one place to another
 - The Buffer is filled with data, then passed along
 - Transfer small chunks of data at a time
-
 Stream 
-
 - Overall stream of data that flows overtime from one place to another
 -Can create streams in Node.js to transfer data
 -Increase Performance
-
-
 */
 
 
 // Tutorial 14, Readable Streams
 
 /*
-
 Different Kind of streams
-
 -Writeable Streams - allow node.js to write data to astream
-
 -Readbable Streams - allow node.js to read data from a stream
-
 -Duplex - can read and write to a stream
-
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
